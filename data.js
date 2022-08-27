808c0e3e8b5995fa052b45ea07a0094759e04aef
mohandand <mohandand@gmail.com>
2022-08-26 22:14:41 -0700

    1 file added

---
+++ data.js
@@ -0,0 +1,120 @@
+export const data = {
+  type: "quiz",
+  questions: [
+    {
+      id: 1,
+      descrition: "What is HTML?",
+      options: [
+        {
+          id: 1,
+          description: "Hyper"
+        },
+        {
+          id: 2,
+          description: "Text"
+        },
+        {
+          id: 3,
+          description: "Markp"
+        },
+        {
+          id: 4,
+          description: "Language"
+        }
+      ],
+      answerId: 2
+    },
+    {
+      id: 2,
+      descrition: "What is HTML2?",
+      options: [
+        {
+          id: 1,
+          description: "Hyper2"
+        },
+        {
+          id: 2,
+          description: "Text2"
+        },
+        {
+          id: 3,
+          description: "Markp2"
+        },
+        {
+          id: 4,
+          description: "Language2"
+        }
+      ],
+      answerId: 3
+    },
+    {
+      id: 3,
+      descrition: "What is HTML3?",
+      options: [
+        {
+          id: 1,
+          description: "Hyper3"
+        },
+        {
+          id: 2,
+          description: "Text3"
+        },
+        {
+          id: 3,
+          description: "Markp3"
+        },
+        {
+          id: 4,
+          description: "Language3"
+        }
+      ],
+      answerId: 1
+    },
+    {
+      id: 4,
+      descrition: "What is HTML4?",
+      options: [
+        {
+          id: 1,
+          description: "Hyper4"
+        },
+        {
+          id: 2,
+          description: "Text4"
+        },
+        {
+          id: 3,
+          description: "Markp4"
+        },
+        {
+          id: 4,
+          description: "Language4"
+        }
+      ],
+      answerId: 2
+    },
+    {
+      id: 5,
+      descrition: "What is HTML5?",
+      options: [
+        {
+          id: 1,
+          description: "Hyper5"
+        },
+        {
+          id: 2,
+          description: "Text5"
+        },
+        {
+          id: 3,
+          description: "Markp5"
+        },
+        {
+          id: 4,
+          description: "Language5"
+        }
+      ],
+      answerId: 4
+    }
+  ]
+};
