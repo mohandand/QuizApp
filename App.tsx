e96999259c8b24fbe0d66b5b87f78c59cac3edab
mohandand <mohandand@gmail.com>
2022-08-28 23:09:04 -0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import Question from './components/Questions.js';
 export default function App() {
   return (
     <div>
-      <h1>QuizAPP</h1>
+      <h1>Quiz APP</h1>
       <Question />
     </div>
   );
