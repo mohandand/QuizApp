91f629985795cf847192b3b987a0ea8f0af51d8d
mohandand <mohandand@gmail.com>
2022-09-05 19:20:51 -0700

    1 file modified

--- components/Questions.js
+++ components/Questions.js
@@ -4,7 +4,14 @@ import { data } from '../data.js';
 export default function Question() {
   let question = data.questions;
   let allQues = question.map((item, index) => {
-    return <div>{item.descrition}</div>;
+    return (
+      <div>
+        {item.descrition}
+        {item.options.map(() => {
+          return <div></div>;
+        })}
+      </div>
+    );
   });
   return <div>{allQues}</div>;
 }
